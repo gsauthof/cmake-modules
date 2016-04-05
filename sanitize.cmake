d9583711ce2e609b69461210bb7c8ddaa435db87
Georg Sauthoff <mail@georg.so>
2016-04-05 09:41:58 +0200

    Include -g in SanitizeO
    for prettier stack traces ...

--- sanitize.cmake
+++ sanitize.cmake
@@ -22,9 +22,9 @@ MARK_AS_ADVANCED(
 # cf. http://clang.llvm.org/docs/AddressSanitizer.html#usage
 
 SET(CMAKE_CXX_FLAGS_SANITIZEO
-    "${CMAKE_CXX_FLAGS_RELEASE} -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined")
+    "${CMAKE_CXX_FLAGS_RELEASE} -g -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined")
 SET(CMAKE_C_FLAGS_SANITIZEO
-    "${CMAKE_C_FLAGS_RELEASE} -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined")
+    "${CMAKE_C_FLAGS_RELEASE} -g -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined")
 SET(CMAKE_EXE_LINKER_FLAGS_SANITIZEO "${CMAKE_EXE_LINKER_FLAGS_RELEASE}")
 SET(CMAKE_SHARED_LINKER_FLAGS_SANITIZEO "${CMAKE_SHARED_LINKER_FLAGS_RELEASE}")
 SET(CMAKE_STATIC_LINKER_FLAGS_SANITIZEO "${CMAKE_STATIC_LINKER_FLAGS_RELEASE}")
