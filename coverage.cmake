dc62c12f2cbc47dba764245b35c0e938cff17c16
Georg Sauthoff <mail@georg.so>
2016-04-18 21:39:08 +0200

    Add CoverageO build type and comments

--- coverage.cmake
+++ coverage.cmake
@@ -1,5 +1,11 @@
 message("Defining coverage build type ...")
 
+# - `-fprofile-arcs` and `-ftest-coverage` are understood by GCC and Clang
+# - `--coverage` is an alias for those two options, also understood
+#   by GCC and Clang
+# - `-fprofile-arcs` (or `--coverage`) must also be specified at link time
+# - specifying `-test-coverage` at link time doesn't hurt
+# - cmake automatically supplied CXX_FLAGS or C_FLAGS at link time
 SET(CMAKE_CXX_FLAGS_COVERAGE
     "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage"
     )
@@ -21,5 +27,38 @@ MARK_AS_ADVANCED(
     CMAKE_MODULE_LINKER_FLAGS_COVERAGE
     )
 
-# lcov --capture --directory ./ --output-file coverage.info
-# genhtml coverage.info --output-directory pages
+# - mixing coverage gathering and optimization is supported, but
+#   but may yield some inaccuracies
+SET(CMAKE_CXX_FLAGSCOVERAGEO
+    "${CMAKE_CXX_FLAGS_RELEASE} -fprofile-arcs -ftest-coverage"
+    )
+SET(CMAKE_C_FLAGSCOVERAGEO
+    "${CMAKE_C_FLAGS_RELEASE} -fprofile-arcs -ftest-coverage"
+    )
+SET(CMAKE_EXE_LINKER_FLAGSCOVERAGEO "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
+SET(CMAKE_SHARED_LINKER_FLAGSCOVERAGEO "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
+SET(CMAKE_STATIC_LINKER_FLAGSCOVERAGEO "${CMAKE_STATIC_LINKER_FLAGS_DEBUG}")
+SET(CMAKE_MODULE_LINKER_FLAGSCOVERAGEO "${CMAKE_MODULE_LINKER_FLAGS_DEBUG}")
+
+# for the GUI
+MARK_AS_ADVANCED(
+    CMAKE_CXX_FLAGSCOVERAGEO
+    CMAKE_C_FLAGSCOVERAGEO
+    CMAKE_EXE_LINKER_FLAGSCOVERAGEO
+    CMAKE_SHARED_LINKER_FLAGSCOVERAGEO
+    CMAKE_STATIC_LINKER_FLAGSCOVERAGEO
+    CMAKE_MODULE_LINKER_FLAGSCOVERAGEO
+    )
+
+
+# Example post-processing with lcov:
+#
+#     lcov --capture --directory ./ --output-file lcov.info
+#     lcov --remove lcov.info 'boost/*' '/c++/*' 'build-cov/*' 'cppformat/*' \
+#        /qt5/*' -o lcov_mod.info
+#     mv lcov_mod.info lcov.info
+#     genhtml lcov.info --output-directory pages
+#
+# View e.g. via:
+#
+#     firefox pages/index.html
