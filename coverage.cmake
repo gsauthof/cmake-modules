72c2974b26398a3122c879ff35f5d7075fce5b04
Georg Sauthoff <mail@georg.so>
2017-04-30 10:29:27 +0200

    simplify coverage switches, update lcov notes

--- coverage.cmake
+++ coverage.cmake
@@ -1,17 +1,14 @@
 message("Defining coverage build type ...")
 
-# - `-fprofile-arcs` and `-ftest-coverage` are understood by GCC and Clang
-# - `--coverage` is an alias for those two options, also understood
-#   by GCC and Clang
-# - `-fprofile-arcs` (or `--coverage`) must also be specified at link time
-# - specifying `-test-coverage` at link time doesn't hurt
-# - cmake automatically supplied CXX_FLAGS or C_FLAGS at link time
-SET(CMAKE_CXX_FLAGS_COVERAGE
-    "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage"
-    )
-SET(CMAKE_C_FLAGS_COVERAGE
-    "${CMAKE_C_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage"
-    )
+# - --coverage is equivalent to the old -fprofile-arcs -ftest-coverage
+#   combination
+# - --coverage must be specified for each compile and link step
+#   (i.e. CFLAGS/CXXFLAGS and LDFLAGS)
+# - cmake automatically supplies CXX_FLAGS or C_FLAGS at link time
+# - --coverage is understood by GCC and clang
+# - clang also support an alternative coverage collection/processing scheme
+SET(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} --coverage")
+SET(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} --coverage")
 SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
 SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
 SET(CMAKE_STATIC_LINKER_FLAGS_COVERAGE "${CMAKE_STATIC_LINKER_FLAGS_DEBUG}")
@@ -25,16 +22,12 @@ MARK_AS_ADVANCED(
     CMAKE_SHARED_LINKER_FLAGS_COVERAGE
     CMAKE_STATIC_LINKER_FLAGS_COVERAGE
     CMAKE_MODULE_LINKER_FLAGS_COVERAGE
-    )
+)
 
 # - mixing coverage gathering and optimization is supported, but
 #   but may yield some inaccuracies
-SET(CMAKE_CXX_FLAGSCOVERAGEO
-    "${CMAKE_CXX_FLAGS_RELEASE} -fprofile-arcs -ftest-coverage"
-    )
-SET(CMAKE_C_FLAGSCOVERAGEO
-    "${CMAKE_C_FLAGS_RELEASE} -fprofile-arcs -ftest-coverage"
-    )
+SET(CMAKE_CXX_FLAGSCOVERAGEO "${CMAKE_CXX_FLAGS_RELEASE} --coverage")
+SET(CMAKE_C_FLAGSCOVERAGEO "${CMAKE_C_FLAGS_RELEASE} --coverage")
 SET(CMAKE_EXE_LINKER_FLAGSCOVERAGEO "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
 SET(CMAKE_SHARED_LINKER_FLAGSCOVERAGEO "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}")
 SET(CMAKE_STATIC_LINKER_FLAGSCOVERAGEO "${CMAKE_STATIC_LINKER_FLAGS_DEBUG}")
@@ -48,16 +41,20 @@ MARK_AS_ADVANCED(
     CMAKE_SHARED_LINKER_FLAGSCOVERAGEO
     CMAKE_STATIC_LINKER_FLAGSCOVERAGEO
     CMAKE_MODULE_LINKER_FLAGSCOVERAGEO
-    )
+)
 
 
 # Example post-processing with lcov:
 #
-#     lcov --capture --directory ./ --output-file lcov_raw.info
-#     lcov --remove lcov_raw.info 'boost/*' '/c++/*' 'build-cov/*' 'cppformat/*' \
-#        '/qt5/*' -o lcov.info
-#     genhtml lcov.info --output-directory pages
+# see also gen-coverage.py
+#
+#     lcov --rc lcov_branch_coverage=1 --capture --directory $PWD -o coverage_post_raw.info
+#     lcov --capture --initial --directory $PWD -o coverage_init_raw.info
+#     lcov --rc lcov_branch_coverage=1 --remove coverage_post_raw.info 'boost/*' '/c++/*' 'build-cov/*' 'cppformat/*' '/qt5/*' -o coverage_post.info
+#     lcov --rc lcov_branch_coverage=1 --remove coverage_init_raw.info 'boost/*' '/c++/*' 'build-cov/*' 'cppformat/*' '/qt5/*' -o coverage_init.info
+#     lcov --rc lcov_branch_coverage=1  -a coverage_init.info -a coverage_post.info -o coverage.info
+#     genhtml --branch-coverage coverage.info -o coverage
 #
 # View e.g. via:
 #
-#     firefox pages/index.html
+#     firefox coverage/index.html
